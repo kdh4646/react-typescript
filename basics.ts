da97851793a5ddc2794e5f9641d23c592b50e60f
Derek Kweon <kdh4646@hanmail.net>
2023-10-05 23:10:57 -0700

    2023-10-05 typescript basic info

---
+++ basics.ts
@@ -0,0 +1,37 @@
+/*
+    Primitives: start with 'Upper case' is Object, 'Lower case' is type
+    ex) Number: Object, number: type
+*/
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+//More complex types
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+//Object
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+//Array of Objects
+let people: {
+  name: string;
+  age: number;
+}[];
