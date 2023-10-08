3c6e56b7ce9726b38b325a4fda6b452afc776bf1
Derek Kweon <kdh4646@hanmail.net>
2023-10-07 23:19:58 -0700

    2023-10-07 added models and components

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,10 +1,18 @@
-function Todos() {
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
+/*
+    FC stands for Function Components
+    React.FC is generic type
+*/
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      <li>Learn Reat</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
